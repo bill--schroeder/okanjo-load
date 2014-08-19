36afaff3dd0b882b0cf435e04317bf8998fe35d1
bill--schroeder <bill@zealocity.com>
2014-08-19 12:31:48 -0500

    updated product load files and import to use new values

--- config.sandbox.js
+++ config.sandbox.js
@@ -14,8 +14,8 @@ module.exports = exports = {
     productData: {
       storeId: 5170,
       causeId: null,
-      sourceFile: "data/SSI Product File - Office - 20140619.csv",
-      //deleteAllRoute: "https://sandbox-api.okanjo.com/products?key=TEST-AKEfqKSWps6QpDLVuZChWim3&store_id=5170&page_size=900&signature=1506bd11cab07eb1b2c1907c24508646c600ddbb72b17285bd871812d0c9b687"
-      deleteAllRoute: "https://sandbox-api.okanjo.com/products?key=TEST-AKEfqKSWps6QpDLVuZChWim3&store_id=5173&page_size=900&signature=3f45bf76f92e282b18e4e6074dbb153973f73a940a5f4a57961fee59f063c890"
+      sourceFile: "data/SSI Product File - 20140818.csv",
+      deleteAllRoute: "https://sandbox-api.okanjo.com/products?key=TEST-AKEfqKSWps6QpDLVuZChWim3&store_id=5170&page_size=900&signature=1506bd11cab07eb1b2c1907c24508646c600ddbb72b17285bd871812d0c9b687"
+      //deleteAllRoute: "https://sandbox-api.okanjo.com/products?key=TEST-AKEfqKSWps6QpDLVuZChWim3&store_id=5173&page_size=900&signature=3f45bf76f92e282b18e4e6074dbb153973f73a940a5f4a57961fee59f063c890"
     }
 };
